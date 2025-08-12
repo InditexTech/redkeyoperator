4534a2a5d6cb22503f935b1a811d198b8cabcc73
Alberto Martínez Pérez <albertompe@ext.inditex.com>
2025-08-12 16:17:34 +0200

    Rename webhook

--- webhook/v1/rediscluster_webhook.go
+++ webhook/v1/rediscluster_webhook.go
@@ -8,16 +8,16 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
-	redisv1 "github.com/inditextech/redisoperator/api/v1"
+	redkeyv1 "github.com/inditextech/redkeyoperator/api/v1"
 )
 
 // nolint:unused
 // log is for logging in this package.
-var redisclusterlog = logf.Log.WithName("rediscluster-resource")
+var redkeyclusterlog = logf.Log.WithName("redkeycluster-resource")
 
-// SetupRedisClusterWebhookWithManager registers the webhook for RedisCluster in the manager.
-func SetupRedisClusterWebhookWithManager(mgr ctrl.Manager) error {
-	redisclusterlog.Info("Setting up RedisCluster webhook with manager")
-	return ctrl.NewWebhookManagedBy(mgr).For(&redisv1.RedisCluster{}).
+// SetupRedKeyClusterWebhookWithManager registers the webhook for RedKeyCluster in the manager.
+func SetupRedKeyClusterWebhookWithManager(mgr ctrl.Manager) error {
+	redkeyclusterlog.Info("Setting up RedKeyCluster webhook with manager")
+	return ctrl.NewWebhookManagedBy(mgr).For(&redkeyv1.RedKeyCluster{}).
 		Complete()
 }
